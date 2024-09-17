8ab5493b93b33d01ce172bc257f9dc2646ead8bb
Craig Foote <CraigFoote@gmail.com>
2024-09-17 11:11:51 -0400

    Fix yard conversions.

--- converter.length/src/main/java/converter/length/YdKeyListener.java
+++ converter.length/src/main/java/converter/length/YdKeyListener.java
@@ -47,32 +47,32 @@ public class YdKeyListener extends KeyAdapter {
 
 		// update millimeters
 		this.lengthPanel.mmText.removeKeyListener(this.lengthPanel.mmListener);
-		this.lengthPanel.mmText.setText(isValid ? formatter.format(input * 1.609e+6) : EMPTY);
+		this.lengthPanel.mmText.setText(isValid ? formatter.format(input * 914.4) : EMPTY);
 		this.lengthPanel.mmText.addKeyListener(this.lengthPanel.mmListener);
 
 		// update centimeters
 		this.lengthPanel.cmText.removeKeyListener(this.lengthPanel.cmListener);
-		this.lengthPanel.cmText.setText(isValid ? formatter.format(input * 160900) : EMPTY);
+		this.lengthPanel.cmText.setText(isValid ? formatter.format(input * 91.44) : EMPTY);
 		this.lengthPanel.cmText.addKeyListener(this.lengthPanel.cmListener);
 
 		// update meters
 		this.lengthPanel.mText.removeKeyListener(this.lengthPanel.mListener);
-		this.lengthPanel.mText.setText(isValid ? formatter.format(input * 1609) : EMPTY);
+		this.lengthPanel.mText.setText(isValid ? formatter.format(input / 1.094) : EMPTY);
 		this.lengthPanel.mText.addKeyListener(this.lengthPanel.mListener);
 
 		// update kilometers
 		this.lengthPanel.kmText.removeKeyListener(this.lengthPanel.kmListener);
-		this.lengthPanel.kmText.setText(isValid ? formatter.format(input * 1.609) : EMPTY);
+		this.lengthPanel.kmText.setText(isValid ? formatter.format(input / 1094) : EMPTY);
 		this.lengthPanel.kmText.addKeyListener(this.lengthPanel.kmListener);
 
 		// update inches
 		this.lengthPanel.inText.removeKeyListener(this.lengthPanel.inListener);
-		this.lengthPanel.inText.setText(isValid ? formatter.format(input * 63360) : EMPTY);
+		this.lengthPanel.inText.setText(isValid ? formatter.format(input * 36) : EMPTY);
 		this.lengthPanel.inText.addKeyListener(this.lengthPanel.inListener);
 
 		// update feet
 		this.lengthPanel.ftText.removeKeyListener(this.lengthPanel.ftListener);
-		this.lengthPanel.ftText.setText(isValid ? formatter.format(input * 5280) : EMPTY);
+		this.lengthPanel.ftText.setText(isValid ? formatter.format(input * 3) : EMPTY);
 		this.lengthPanel.ftText.addKeyListener(this.lengthPanel.ftListener);
 
 		// update miles
